a3b38a3b1ea046c32c745ab9857f39fca8e9927e
Marco Chessa <marco.chessa@chessa.co.uk>
2022-11-04 01:41:43 +0000

    setup for VScode

--- main.tf
+++ main.tf
@@ -30,11 +30,124 @@ resource "azurerm_resource_group" "rg" {
   }
 }
 
-
 # Create a virtual network
 resource "azurerm_virtual_network" "vnet" {
-  name                = "myTFVnet"
+  name                = var.virtual_network_name
   address_space       = ["10.0.0.0/16"]
-  location            = "eastus"
+  location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
+}
+
+# Create a subnet
+resource "azurerm_subnet" "sb" {
+  name                 = var.subnet_name
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+# Add security group
+resource "azurerm_network_security_group" "sg" {
+  name                = var.security_group_name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  tags = {
+    environment = "Production"
+  }
+
+}
+
+# Add an inbound security rule - locked to MC public IP
+resource "azurerm_network_security_rule" "prod-rule" {
+  name                        = var.network_security_rule_name
+  priority                    = 100
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "*"
+  source_port_range           = "*"
+  destination_port_range      = "*"
+  source_address_prefix       = "90.212.155.243/32"
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_resource_group.rg.name
+  network_security_group_name = azurerm_network_security_group.sg.name
+
+}
+
+# Associate the security group to the subnet
+resource "azurerm_subnet_network_security_group_association" "sga" {
+  subnet_id                 = azurerm_subnet.sb.id
+  network_security_group_id = azurerm_network_security_group.sg.id
+}
+
+# Create a dynamic public IP
+resource "azurerm_public_ip" "pub-ip" {
+  name                = var.public_ip
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  allocation_method   = "Dynamic"
+
+  tags = {
+    environment = "Production"
+  }
+}
+
+# Create a network interface
+resource "azurerm_network_interface" "nic" {
+  name                = var.network_interface
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.sb.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.pub-ip.id
+  }
+
+  tags = {
+    environment = "Production"
+  }
+}
+
+# Create a Linux Virtual Machine
+resource "azurerm_linux_virtual_machine" "linux-vm" {
+  name                  = var.linux_virtual_machine
+  resource_group_name   = azurerm_resource_group.rg.name
+  location              = azurerm_resource_group.rg.location
+  size                  = "Standard_B1s"
+  admin_username        = "adminuser"
+  network_interface_ids = [azurerm_network_interface.nic.id]
+
+  custom_data = filebase64("customdata.tpl")
+
+  admin_ssh_key {
+    username   = "adminuser"
+    public_key = file("~/.ssh/id_rsa.pub")
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
+  }
+
+  provisioner "local-exec" {
+    command = templatefile("mac-ssh-script.tpl", {
+      hostname     = self.public_ip_address,
+      user         = "adminuser",
+      identityfile = "~/.ssh/id_rsa"
+    })
+    interpreter = ["bash", "-c"]
+  }
+
+  tags = {
+    environment = "Production"
+  }
 }
--- variables.tf
+++ variables.tf
@@ -1,3 +1,32 @@
 variable "resource_group_name" {
   default = "myTFResourceGroup"
 }
+
+variable "virtual_network_name" {
+  default = "myTFVnet"
+}
+
+variable "subnet_name" {
+  default = "myTFSubnet"
+}
+
+variable "security_group_name" {
+  default = "myTFSecurityGroup"
+}
+
+variable "network_security_rule_name" {
+  default = "myTFProdRule"
+}
+
+variable "public_ip" {
+  default = "myTFPublicIP"
+}
+
+variable "network_interface" {
+  default = "myTFNetworkInterface"
+}
+
+variable "linux_virtual_machine" {
+  default = "myTFLinuxVirtualMachine"
+}
+
